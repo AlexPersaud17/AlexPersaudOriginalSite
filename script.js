ba47f43b72e2c75ecd206ab9caa450fbb474c97e
Alex Persaud <alex_persaud01@yahoo.com>
2017-04-13 15:45:52 -0400

    JS testing on live site

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
+	window.location = 'index.html';
 	$('#home-logo').fadeIn(1500).delay(1000).fadeOut(1500)
 	$('#welcome').delay(4000).fadeIn(1500).fadeOut(1500)
 	$('header').fadeIn(500)
 	$('.body-wrapper').delay(250).fadeIn(1000)
 
-	if(window.location.href.indexOf('/') > -1){
+	if(window.location.href.indexOf('index.html') > -1){
 		setTimeout(function(){ window.location = 'about.html'; }, 7000);
 	}
 });
