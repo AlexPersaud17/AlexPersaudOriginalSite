38f66b144b2d335ffc330320db367ba2af90855b
Alex Persaud <alex_persaud01@yahoo.com>
2017-04-14 00:20:08 -0400

    Image CSS with JS hover

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ $(document).ready(function(){
 	$('.body-wrapper').delay(250).fadeIn(1000)
 
 	if(window.location.href == "https://alexpersaud17.github.io/" || window.location.href == "https://alexpersaud17.github.io/index.html"){
-		setTimeout(function(){ window.location = "https://alexpersaud17.github.io/about.html"; }, 7000);
+		setTimeout(function(){ window.location = "https://alexpersaud17.github.io/about.html"; }, 7000)
 	}
+
+	if(window.location.href.indexOf("index.html") > -1){
+		setTimeout(function(){ window.location = "about.html"; }, 7000)
+	}
+
+	$(".fun-pics").mouseenter(function(){
+		$(this).fadeTo(200, 0.5)
+		$(this).position();
+	});
+	$(".fun-pics").mouseleave(function(){
+		$(this).fadeTo(200, 1)
+	});
+
 });
